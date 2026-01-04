b282ce244ced97dd67369500c2f9438c25401fcc
Vaibhav14k <157138717+Vaibhav14k@users.noreply.github.com>
2026-01-04 10:07:06 +0530

    Delete Leetcode-Pattern-problems-main/Leetcode-Pattern-problems-main directory
    Here is by mistake add this file

--- Leetcode-Pattern-problems-main/Leetcode-Pattern-problems-main/Bit Manipulation/right_shift.py
+++
@@ -1,8 +0,0 @@
-a = 6
-print("a-->",bin(a)[2:])
-print("length of bits-->",a.bit_length())
-right_shift = a>>1
-print("right shift by 1 pos-->",bin(right_shift)[2:])
-
-multiply_by_2 = a>>1
-print("Multiply by 2-->",multiply_by_2)
