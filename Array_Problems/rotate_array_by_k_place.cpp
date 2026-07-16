b2fbad4c4ea338c1306ae74c883d5ee2319fb27d
Vaibhav <vaibhavkangane1424@gmail.com>
2026-07-16 15:59:53 +0530

    solve the problem : rotate the array by k place or d distance

--- Array_Problems/rotate_array_by_k_place.cpp
+++ Array_Problems/rotate_array_by_k_place.cpp
@@ -1,1 +1,28 @@
-#include
+#include<iostream>
+#include<vector>
+using namespace std; 
+void rotatearray(vector<int> &arr,int d){
+    int n = arr.size();
+    d = d % n;
+    vector<int> temp(d);        
+    for(int i=0;i<d;i++){
+        temp[i] = arr[i];
+        cout<<temp[i]<<" ";
+    }
+    cout<<endl;
+    for( int i= d ; i< n; i++){
+        arr[i-d] = arr[i];
+    }
+    for(int i=n-d;i<n;i++){
+        arr[i]= temp[i-d-1];
+    }
+}
+int main(){
+    vector<int> arr = { 1,2,3,4,5,6,7};
+    int d = 3;
+    int n = arr.size();
+    rotatearray(arr,d);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
